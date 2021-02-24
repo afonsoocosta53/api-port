2dff556997a220ad18929aa3672c5027dcc6ce8c
acoesdividendos <acoesdividendos@gmail.com>
2021-02-24 17:50:35 +0000

    change db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 module.exports = {
-  'secret': 'devdacticIsAwesome',
-  'secret2': 'chourica',
-  'database': 'mongodb://portfollium:portfollium2018@ds125723.mlab.com:25723/portfollium',
-  'email': 'ourkartal@gmail.com',
-  'email_password': 'filipalinda'
+  secret: "devdacticIsAwesome",
+  secret2: "chourica",
+  database:
+    "mongodb+srv://portfollium:portfollium@cluster0.mffry.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  email: "ourkartal@gmail.com",
+  email_password: "filipalinda",
 };
